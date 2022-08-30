8b51992366a6c27713aed1d4d7c9571aa06e6544
Андрей Легкий <legkiy.andrey@bk.ru>
2022-08-30 15:41:26 +0300

    Проект

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -17,15 +17,13 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(
-      User, 
-      on_delete=models.CASCADE, 
+      User,
+      on_delete=models.CASCADE,
       related_name="posts",
- ) 
+    )
     group = models.ForeignKey(Group, on_delete=models.SET_NULL,
                               related_name="posts", blank=True, null=True)
 
 
 class Meta:
-    index = [
-            models.Index(fields=['-pub_date'],)
-    ]
+    index = [models.Index(fields=['-pub_date'])]
