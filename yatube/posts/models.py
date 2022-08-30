194f800d0f41e239ae103b3eb0584f19af5aaa9f
Андрей Легкий <legkiy.andrey@bk.ru>
2022-08-30 15:44:22 +0300

    Проект

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -17,9 +17,9 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(
-      User,
-      on_delete=models.CASCADE,
-      related_name="posts",
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
     )
     group = models.ForeignKey(Group, on_delete=models.SET_NULL,
                               related_name="posts", blank=True, null=True)
